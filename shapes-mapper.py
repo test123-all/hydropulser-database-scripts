5ae4097f60a23357c74bb55af726ca3dc34fac67
Preuß, Nils <nils.preuss@fst.tu-darmstadt.de>
2023-02-02 14:26:40 +0100

    Update profile shapes and validation

--- shapes-mapper.py
+++ shapes-mapper.py
@@ -1,42 +1,28 @@
 import os
 from pyshacl import validate
 from rdflib import Graph
-from rdflib.namespace import RDF, SH
 
-g = Graph()
-g.parse("shapes/Thing.shacl.ttl")
+# VALIDATION OF METADATA PROFILES
+shapes_location = "shape-cache"
+meta_shapefiles = ["AIMS-AP.shacl.ttl", "shacl-shacl.ttl"]
+shape_graph = Graph()
+for shapefile in meta_shapefiles:
+    shape_graph.parse(f"{shapes_location}/{shapefile}")
 
-# fail if no shape found
-shapes = g[: RDF.type: SH.NodeShape]
-try:
-    shape = next(shapes)
-except StopIteration:
-    raise LookupError("no shapes found in source file!")
+with os.scandir(shapes_location) as shapefiles:
+    for shapefile in shapefiles:
+        if shapefile.name in meta_shapefiles:
+            print(f"skipping meta shape {shapefile.name}\n")
+            continue
+        if shapefile.name.endswith(".ttl") and shapefile.is_file():
 
-# fail if another shape found
-try:
-    next(shapes)
-except StopIteration:
-    pass
-else:
-    raise LookupError("more than one shape found in source file!")
-
-
-for o in g[shape: SH.property]:
-    print(o)
-
-
-# VALIDATION
-
-with os.scandir("shapes") as it:
-    for entry in it:
-        if entry.name.endswith(".ttl") and entry.is_file():
-
-            print(entry.path)
-            r = validate(entry.path,
+            print(shapefile.path)
+            r = validate(shapefile.path,
                          data_graph_format="ttl",
+                         shacl_graph=shape_graph,
                          inference="rdfs",
+                         advanced=True,
                          debug=False)
-            conforms, results_graph, results_text = r
+            _, _, results_text = r
 
             print(results_text)
